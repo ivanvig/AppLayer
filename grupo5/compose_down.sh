d7e7cf3f0801928b5123eaa292e9562be8c59542
SorianoJuan <ju_soriano88@hotmail.com>
2018-06-05 14:19:50 -0300

    docker-compose de networks

---
+++ grupo5/compose_down.sh
@@ -0,0 +1,10 @@
+#Docker compose de toda la red
+
+# Nginx
+cd nginx
+docker-compose down
+cd ..
+# Strapi
+cd strapi-docker-master
+docker-compose down
+cd ..
---
+++ grupo5/compose_up.sh
@@ -0,0 +1,10 @@
+#Docker compose de toda la red
+
+# Nginx
+cd nginx
+docker-compose up
+cd ..
+# Strapi
+cd strapi-docker-master
+docker-compose up
+cd ..
