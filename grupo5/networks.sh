5623ff8c0583bb9b67b04182fd94d351967a6547
JuanSoriano <kwfv4fps>
2018-06-06 01:22:47 -0300

    networks.sh + gitignore strapi

---
+++ grupo5/networks.sh
@@ -0,0 +1,8 @@
+docker network create --driver bridge --ipv6 --subnet 2001:a:1::/64 net0
+docker network create --driver bridge --ipv6 --subnet 2001:a:2::/64 net1
+docker network create --driver bridge --ipv6 --subnet 2001:d:1::/64 redD
+docker network create --driver bridge --ipv6 --subnet 2001:a:3::/64 red3
+docker network create --driver bridge --ipv6 --subnet 2001:a:4::/64 red4
+docker network create --driver bridge --ipv6 --subnet 2001:a:5::/64 red5
+docker network create --driver bridge --ipv6 --subnet 2001:a:6::/64 red6
+docker network create --driver bridge --ipv6 --subnet 2001:a:7::/64 red7
