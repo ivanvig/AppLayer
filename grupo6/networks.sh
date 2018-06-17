d01bd2c5b0599ece235c6cdb401351cd9849efd2
root <root@localhost.localdomain>
2018-06-17 18:04:13 -0300

    networks sin ipv4 ni ipam=default

--- grupo6/networks.sh
+++ grupo6/networks.sh
@@ -1,6 +1,6 @@
-docker network create --driver bridge --ipv6 --ipam-driver=default --subnet 2001:b:1::/64 --subnet 192.168.2.0/24 net0
+docker network create --driver bridge --ipv6 --subnet 2001:b:1::/64 net0
 docker network create --driver bridge --ipv6 --subnet 2001:d:1::/64 redD
-docker network create --driver bridge --ipv6 --ipam-driver=default --subnet 2001:b:2::/64 red2
+docker network create --driver bridge --ipv6 --subnet 2001:b:2::/64 red2
 docker network create --driver bridge --ipv6 --subnet 2001:b:3::/64 red3
 docker network create --driver bridge --ipv6 --subnet 2001:b:4::/64 red4
 docker network create --driver bridge --ipv6 --subnet 2001:b:5::/64 red5
