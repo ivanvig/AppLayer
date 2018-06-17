6c86b04324142a7e4df3a73c22ef083b033137a1
root <root@localhost.localdomain>
2018-06-17 20:52:36 -0300

    cambio al grupo 6

--- grupo6/networks.sh
+++ grupo6/networks.sh
@@ -1,5 +1,5 @@
 docker network create --driver bridge --ipv6 --subnet 2001:b:1::/64 net0
-docker network create --driver bridge --ipv6 --subnet 2001:b:2::/64 net1
+docker network create --driver bridge --ipv6 --subnet 2001:b:2::/64 red2
 docker network create --driver bridge --ipv6 --subnet 2001:d:1::/64 redD
 docker network create --driver bridge --ipv6 --subnet 2001:b:3::/64 red3
 docker network create --driver bridge --ipv6 --subnet 2001:b:4::/64 red4
