b0254c0956eb81e65029b485108c60fb78d7e21f
root <root@localhost.localdomain>
2018-06-17 23:09:26 -0300

    fix webmin

--- grupo5/bgp-ospf/dgw.sh
+++ grupo5/bgp-ospf/dgw.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+echo -e "\e[1;33m BGP-OSPF \e[0m"
 docker exec -it bgpospf_b1_1 ip -6 r del default
 docker exec -it bgpospf_r3_1 ip -6 r del default
 docker exec -it bgpospf_r5_1 ip -6 r del default
@@ -13,12 +15,14 @@ docker exec -it bgpospf_h11_1 ip -6 r add default via 2001:a:5::10
 docker exec -it bgpospf_h13_1 ip -6 r add default via 2001:a:6::10
 
 #Nginx
+echo -e "\e[1;36m Nginx \e[0m"
 docker exec -it nginx_web_1 apt-get update
 docker exec -it nginx_web_1 apt-get install iproute
 docker exec -it nginx_web_1 ip -6 r del default
 docker exec -it nginx_web_1 ip -6 r add default via 2001:a:2::10
 
 #Webmin
+echo -e "\e[1;36m Webmin \e[0m"
 docker exec -it webminbind_bind_1 apt-get update
 docker exec -it webminbind_bind_1 apt-get install iproute
 docker exec -it webminbind_bind_1 ip -6 r del default
