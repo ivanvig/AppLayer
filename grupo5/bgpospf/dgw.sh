4e3fa22ed23f9dbc2285338672f50a38409cae5c
root <root@localhost.localdomain>
2018-06-19 22:02:38 -0300

    ssl en clients

--- grupo5/bgpospf/dgw.sh
+++ grupo5/bgpospf/dgw.sh
@@ -17,13 +17,13 @@ docker exec -it bgpospf_h13_1 ip -6 r add default via 2001:a:6::10
 #Nginx
 echo -e "\e[1;36m Nginx \e[0m"
 docker exec -it nginx_web_1 apt-get update
-docker exec -it nginx_web_1 apt-get install iproute
+docker exec -it nginx_web_1 apt-get -qy install iproute
 docker exec -it nginx_web_1 ip -6 r del default
 docker exec -it nginx_web_1 ip -6 r add default via 2001:a:2::10
 
 #Webmin
 echo -e "\e[1;32m Webmin \e[0m"
 docker exec -it webminbind_bind_1 apt-get update
-docker exec -it webminbind_bind_1 apt-get install iproute
+docker exec -it webminbind_bind_1 apt-get -qy install iproute
 docker exec -it webminbind_bind_1 ip -6 r del default
 docker exec -it webminbind_bind_1 ip -6 r add default via 2001:a:4::10
