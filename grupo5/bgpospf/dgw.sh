b3349073df074c7c6b0887d4a85c529fdc812571
root <root@localhost.localdomain>
2018-06-21 03:31:50 -0300

    curls

--- grupo5/bgpospf/dgw.sh
+++ grupo5/bgpospf/dgw.sh
@@ -6,6 +6,7 @@ docker exec -it bgpospf_r3_1 ip -6 r del default
 docker exec -it bgpospf_r5_1 ip -6 r del default
 docker exec -it bgpospf_r7_1 ip -6 r del default
 docker exec -it bgpospf_h11_1 ip -6 r del default
+docker exec -it bgpospf_h11_1 apt-get -qy install nano
 docker exec -it bgpospf_h13_1 ip -6 r del default
 
 docker exec -it bgpospf_r3_1 ip -6 r add default via 2001:a:3::3
